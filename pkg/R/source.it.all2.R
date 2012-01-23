cb01a793d2df6013567109525e85f958dce4f1ac
martinhecht <martinhecht@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-23 16:33:15 +0000

    Option 'use.iqb.conv' added in source.it.all2.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/eat@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/source.it.all2.R
+++ pkg/R/source.it.all2.R
@@ -11,10 +11,10 @@
 #									wenn FALSE (default) wird höchste Version (development oder stable geladen)
 #	exclude:						character-Vektor mit Modulen, die nicht geladen werden sollen
 #
-# Version: 		0.3.0
+# Version: 		0.4.0
 # Depends: 	
 # Imports: 	
-# Published: 	2011-11-21
+# Published: 	2012-01-23
 # Author: 		Martin Hecht, Christiane Penk
 # Maintainer: 	Martin Hecht, Christiane Penk
 #
@@ -40,7 +40,7 @@
 }
 
 # Funktion
-source.it.all2 <- function ( folder="p:/ZKD/development" , development = TRUE , development.only = FALSE , exclude = NULL ) {
+source.it.all2 <- function ( folder="p:/ZKD/development" , use.zkd.conv = TRUE , development = TRUE , development.only = FALSE , exclude = NULL ) {
 	
 	### Plausichecks
 	# sinnlose Kombi von development und development.only ausschliessen
@@ -48,84 +48,71 @@ source.it.all2 <- function ( folder="p:/ZKD/development" , development = TRUE , 
 			cat ( paste ( "source.it.all: development=FALSE und development.only=TRUE ist nicht sinnvoll.\n" ) )
 			stop ()
 	}
-	
-	# listet alle Dateien auf, die nach den Konventionen benannt sind
-	files <- list.files(folder , all.files=TRUE , pattern = "^.*_{1}\\d*\\.\\d*\\.\\d*\\.[r|R]$", full.names=FALSE )
-	# erster Teil, also part1 (bis zum Unterstrich) wird abgesplittet
-	part1 <- unlist ( strsplit(files, split="_{1}\\d*\\.\\d*\\.\\d*\\.[r|R]$", fixed=FALSE ) )
-	# zweiter Teil, also part2 (Versionsnummer) wird abgesplittet
-	part2 <- 	unname ( mapply ( function ( files , part1 ) {
-			substr(files, nchar(part1)+2 , nchar(files)-2 )
-	} , files , part1 ) )
-	
-	# Plausicheck: Anzahl der Elemente muss in part1 und part2 gleich sein
-	stopifnot ( length ( part1 ) == length ( part2 ) )
-	
+
 	# initialisieren, Sammlung von zu sourcenden files
 	tosource <- NULL	
 	
-	# unique Part1 (mehrfache Dateinamen werden entfernt [also für die Funktion, von denen mehrere Versionsnummern im Ordner lagen])
-	part1.names.unique <- unique ( part1 )
-	
-	# Handling von develop.modules
-	# if ( !is.null ( develop.modules ) ) {
+	# listet alle Dateien auf, die nach den Konventionen benannt sind; oder alle R-files
+	if ( use.zkd.conv ) {
 			
-			# Bezeichnung des ersten Teils der Datei (Funktionsname) abplitten 
-			# dev.vec.part1 <- unlist ( strsplit(develop.modules, split="_{1}\\d*\\.\\d*\\.\\d*$", fixed=FALSE ) )
+			files <- list.files(folder , all.files=TRUE , pattern = "^.*_{1}\\d*\\.\\d*\\.\\d*\\.[r|R]$", full.names=FALSE )
 			
-			# Versionsnummer abplitten
-			# dev.vec.part2 <- unname ( mapply ( function ( develop.modules , dev.vec.part1 ) { 	substr(develop.modules, nchar(dev.vec.part1)+2 , nchar(develop.modules)-2 )
-			# } , develop.modules , dev.vec.part1 ) )
-		
-			# columns die so gesourct werden sollen, wie sie sind (mit Versionsnummer)
-			# as.is.col <- which ( !dev.vec.part2 == "" )
-			# tosource <- c ( tosource , develop.modules[ as.is.col ] )
+			# erster Teil, also part1 (bis zum Unterstrich) wird abgesplittet
+			part1 <- unlist ( strsplit(files, split="_{1}\\d*\\.\\d*\\.\\d*\\.[r|R]$", fixed=FALSE ) )
+			
+			# zweiter Teil, also part2 (Versionsnummer) wird abgesplittet
+			part2 <- unname ( mapply ( function ( files , part1 ) {
+					substr(files, nchar(part1)+2 , nchar(files)-2 )
+			} , files , part1 ) )
+			
+			# Plausicheck: Anzahl der Elemente muss in part1 und part2 gleich sein
+			stopifnot ( length ( part1 ) == length ( part2 ) )
+			
+			# unique Part1 (mehrfache Dateinamen werden entfernt [also für die Funktion, von denen mehrere Versionsnummern im Ordner lagen])
+			part1.names.unique <- unique ( part1 )
+			
+			# für die restlichen Dateinamen (die nicht direkt gesourct werden sollten über develop.modules) die höchste Versionsnummer finden 
+			for ( part1.name in part1.names.unique ) {
+					
+					# alle Dateinamen mit speziellen Namen (part1.name) finden
+					part1.ind <- which ( part1 %in% part1.name )
+					
+					# dazugehörige Versionsnummer
+					part2.temp <- part2 [ part1.ind ]
+					
+					# wenn ein Dateiname noch im develop.modules ist (allerdings jetzt ohne angegebene Versionsnummer), die höchste development Versionsnummer finden
+					# if (  part1.name %in% develop.modules ) {
+							
+							# höchste development Versionsnummer (mit Subfunktion)
+							# vers <- .source.it.all.sort ( part2.temp )
+							
+					# wenn Dateiname nicht in develop.modules, dann für die höchste stable Version finden (d.h. 0 hinten und höchste Nummerierung vorn/mitte)
+					# } else { 
+				
+							# alle, die eine 0 als letzte Ziffer haben
+							if ( !development ) alle0 <- part2.temp [ which ( regexpr ( "^\\d*\\.\\d*\\.0$" , part2.temp ) == 1 ) ] else alle0 <- part2.temp
+				
+							# Warnung wenn keine stable (aber eine develop, die nicht gesourct wird wegen !development) Version vorhanden ist
+							if ( identical ( alle0 , character(0) ) ) cat ( paste ( 
+									"source.it.all: Keine stable Version von" , part1.name , "vorhanden.\n" ) )
 
-			# wenn direkte Versionsnummer im develop.modules angegeben wurde, dann diese Funktionsnamen aus dem Vektor mit den uniquen Dateinamen entfernen
-			# if ( ! identical ( as.is.col , integer(0) ) ) part1.names.unique <- part1.names.unique [ - which ( part1.names.unique %in% dev.vec.part1 [ as.is.col ] ) ]
-	
-	# }
-	
-	# für die restlichen Dateinamen (die nicht direkt gesourct werden sollten über develop.modules) die höchste Versionsnummer finden 
-	for ( part1.name in part1.names.unique ) {
-			
-			# alle Dateinamen mit speziellen Namen (part1.name) finden
-			part1.ind <- which ( part1 %in% part1.name )
-			
-			# dazugehörige Versionsnummer
-			part2.temp <- part2 [ part1.ind ]
-			
-			# wenn ein Dateiname noch im develop.modules ist (allerdings jetzt ohne angegebene Versionsnummer), die höchste development Versionsnummer finden
-			# if (  part1.name %in% develop.modules ) {
+							# sortieren und höchste stable Version bekommen (mit Subfunktion)
+							vers <- .source.it.all.sort ( alle0 )
+							
+							# wenn development.only und ne stable rausgesucht wurde diese löschen
+							if ( development.only & grepl ( ".*\\.0$" , vers ) ) vers <- NA
+							
+							
+					# }
 					
-					# höchste development Versionsnummer (mit Subfunktion)
-					# vers <- .source.it.all.sort ( part2.temp )
+					# wenn der Versionsvektor nicht leer ist, dann an temporäres tosource ranhängen 
+					if ( !is.na ( vers ) ) tosource.temp <- paste ( part1.name , vers , sep = "_" ) else tosource.temp <- NULL
 					
-			# wenn Dateiname nicht in develop.modules, dann für die höchste stable Version finden (d.h. 0 hinten und höchste Nummerierung vorn/mitte)
-			# } else { 
-		
-					# alle, die eine 0 als letzte Ziffer haben
-					if ( !development ) alle0 <- part2.temp [ which ( regexpr ( "^\\d*\\.\\d*\\.0$" , part2.temp ) == 1 ) ] else alle0 <- part2.temp
-		
-					# Warnung wenn keine stable (aber eine develop, die nicht gesourct wird wegen !development) Version vorhanden ist
-					if ( identical ( alle0 , character(0) ) ) cat ( paste ( 
-							"source.it.all: Keine stable Version von" , part1.name , "vorhanden.\n" ) )
-
-					# sortieren und höchste stable Version bekommen (mit Subfunktion)
-					vers <- .source.it.all.sort ( alle0 )
-					
-					# wenn development.only und ne stable rausgesucht wurde diese löschen
-					if ( development.only & grepl ( ".*\\.0$" , vers ) ) vers <- NA
-					
-					
-			# }
-			
-			# wenn der Versionsvektor nicht leer ist, dann an temporäres tosource ranhängen 
-			if ( !is.na ( vers ) ) tosource.temp <- paste ( part1.name , vers , sep = "_" ) else tosource.temp <- NULL
-			
-			#temporäres tosource an tosource ranhänge
-			tosource <- c ( tosource , tosource.temp )
-	
+					#temporäres tosource an tosource ranhänge
+					tosource <- c ( tosource , tosource.temp )
+			}
+	} else {
+			tosource <- files <- list.files(folder , all.files=TRUE , pattern = "^.*[r|R]$", full.names=FALSE )
 	}
 	
 	# excluden
