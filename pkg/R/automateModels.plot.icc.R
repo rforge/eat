c32175ad10e1d04845aa2404dffe5fdfa3ea9303
martinhecht <martinhecht@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-04 06:45:35 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/eat@125 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/automateModels.plot.icc.R
+++ pkg/R/automateModels.plot.icc.R
@@ -7,23 +7,44 @@ automateModels.plot.icc <- function ( results , model.specs ) {
 		
 		fun <- function ( i , results , model.specs ) {
 
-				theta <- get.person.par ( results[i] )$pv.1
+				theta <- get.person.par ( results[i] )[,c("person","pv.1")]
 				
 				dat <- model.specs$dataset[[i]]
-				if ( !is.null ( id.name <- model.specs$id.name[[i]] ) ) dat <- dat [ , - which ( colnames ( dat ) %in% id.name ) ]
+				# if ( !is.null ( id.name <- model.specs$id.name[[i]] ) ) dat <- dat [ , - which ( colnames ( dat ) %in% id.name ) ]
+				id.name <- model.specs$id.name[[i]]
 				if ( !is.null ( cont.names <- model.specs$cont.names[[i]] ) ) dat <- dat [ , - which ( colnames ( dat ) %in% cont.names ) ]
 				dat <- set.col.type ( dat , col.type = list ( "numeric" = NULL ) )
 				
-				# eigentlich ...
-				# np.dich.obj <- np.dich ( dat , theta , thetagrid , progress = F , bwscale = 1.1 , method = "binomial")
+				# theta sortieren wie dat
+				theta <- theta [ na.omit ( match ( dat[,id.name] , theta$person ) ) , ]
 				
-				# in irt.scale (z.B. verwendet für Nawi PilotFE)
-				np.dich.obj <- np.dich ( dat , theta , thetagrid = seq( -3 , 3 , len=100) , progress = F , bwscale = 3 , method = "normal")
+				# nur IDs aus theta auch in Datensatz
+				dat <- dat[dat[,id.name] %in% theta$person,]
+				
+				# ids droppen
+				theta <- theta$pv.1
+				dat <- dat <- dat [ , - which ( colnames ( dat ) %in% id.name ) , drop = FALSE ]
 
+				# Itemparameter
 				item.par <- get.item.par ( results[i] )
+
+				# sortieren wie in dat
+				item.par <- item.par [ na.omit ( match ( colnames(dat) , item.par$item ) ) , ]
+
+				# Datensatz nach item.par$items anpassen
+				dat <- dat[ , colnames(dat) %in% item.par$item , drop = FALSE ]				
+
+				# Parameter
 				b <- item.par$b
 				infit <- item.par$infit
 				outfit <- item.par$outfit
+				
+				# eigentlich ...
+				# np.dich.obj <- np.dich ( dat , theta , thetagrid , progress = F , bwscale = 1.1 , method = "binomial")
+		
+				# in irt.scale (z.B. verwendet für Nawi PilotFE)
+				np.dich.obj <- np.dich ( dat , theta , thetagrid = seq( -3 , 3 , len=100) , progress = F , bwscale = 3 , method = "normal")
+				flush.console()
 				
 				folder.icc <- file.path ( model.specs$folder[[i]] , "icc" )
 				if ( !exists ( folder.icc ) ) temp <- dir.create ( folder.icc )
@@ -33,12 +54,12 @@ automateModels.plot.icc <- function ( results , model.specs ) {
 				# alle in ein pdf
 				pdf ( file.path ( folder.icc , "_ALL_ICC_.pdf" ) )
 				temp <- plot.rasch.np ( np.dich.obj , b , infit , outfit , nsize = 100 , askplot = FALSE , progress = FALSE , 
-									bands = FALSE , plot.b = FALSE , shade = FALSE , shadecol="burlywood1" , icc.pdfs = NULL )
+									bands = TRUE , plot.b = TRUE , shade = FALSE , shadecol="burlywood1" , icc.pdfs = NULL )
 				dev.off()
 				
 				# je Item ein pdf
 				temp <- plot.rasch.np ( np.dich.obj , b , infit , outfit , nsize = 100 , askplot = FALSE , progress = FALSE , 
-									bands = FALSE , plot.b = FALSE , shade = FALSE , shadecol="burlywood1" , icc.pdfs )
+									bands = TRUE , plot.b = TRUE , shade = FALSE , shadecol="burlywood1" , icc.pdfs )
 
 				invisible ( TRUE )
 		}		
