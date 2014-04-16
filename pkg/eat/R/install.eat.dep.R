78228974758ab597fd956991a1577299d86ee002
martinhecht <martinhecht@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-16 08:52:29 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/eat@387 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/eat/R/install.eat.dep.R
+++ pkg/eat/R/install.eat.dep.R
@@ -1,5 +1,5 @@
 install.eat.dep <- function ( ) {
 		cat ( paste0 ( "installing eat dependencies (not on r-forge):" , "\n" ) )
 		pkgs <- c ( "igraph", "survey", "Hmisc", "combinat", "fmsb", "psych", "xlsx", "date", "gdata", "reshape", "sendmailR" , "reshape2" , "stringr" , "ggplot2" , "R.utils" )
-		install.packages ( pkgs , repos = "http://cran.us.r-project.org" )
+		tried <- try ( install.packages ( pkgs , repos = "http://cran.us.r-project.org" ) )
 }
