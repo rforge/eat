68e79b5454b829aa7d0e2fbe6655b807b648ff41
martinhecht <martinhecht@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-11 16:27:28 +0000

    documentation for eatGot added packages to eat:::install.eat.dep() added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/eat@399 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/eat/R/install.eat.dep.R
+++ pkg/eat/R/install.eat.dep.R
@@ -1,5 +1,5 @@
 install.eat.dep <- function ( ) {
 		cat ( paste0 ( "installing eat dependencies (not on r-forge):" , "\n" ) )
-		pkgs <- c ( "igraph", "survey", "Hmisc", "combinat", "fmsb", "psych", "xlsx", "date", "gdata", "reshape", "sendmailR" , "reshape2" , "stringr" , "ggplot2" , "R.utils" )
+		pkgs <- c ( "reshape2" , "igraph", "survey", "Hmisc", "combinat", "fmsb", "psych", "xlsx", "date", "gdata", "sendmailR" , "stringr" , "ggplot2" , "R.utils" , "lme4" , "car" )
 		tried <- try ( install.packages ( pkgs , repos = "http://cran.us.r-project.org" ) )
 }
