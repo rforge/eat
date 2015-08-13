a9fd8803ca5750142a9b6937af6a2d953abea568
nhaag <nhaag@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-13 09:06:13 +0000

    Bugfixes in aggregateData and catPbc
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/eat@468 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/eatPrep/R/aggregateData.R
+++ pkg/eatPrep/R/aggregateData.R
@@ -1,16 +1,15 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # aggregateData - formerly known as zkdDatasetAggregate
 #
 # function:    aggregateData(dat, subunits, units, aggregatemissings = "use.default", rename = FALSE, recodedData = TRUE)
 #
 # Description: rekodiert Datensätze
-# 
-# arguments: 
+#
+# arguments:
 #     dat (data.frame)          ... Datensatz mit ID-Variablen und (mindestens) allen Variablen, die rekodiert werden sollen
-#     subunits (data.frame)     ... ZKD-Inputtabelle für Subunits (Subitems), siehe P:\ZKD\01_Organisation\Konzepte\InputStruktur_Konzept.xlsx   
-#     units (data.frame)        ... ZKD-Inputtabelle für Units (Items), siehe P:\ZKD\01_Organisation\Konzepte\InputStruktur_Konzept.xlsx    
+#     subunits (data.frame)     ... ZKD-Inputtabelle für Subunits (Subitems), siehe P:\ZKD\01_Organisation\Konzepte\InputStruktur_Konzept.xlsx
+#     units (data.frame)        ... ZKD-Inputtabelle für Units (Items), siehe P:\ZKD\01_Organisation\Konzepte\InputStruktur_Konzept.xlsx
 #     aggregatemissings(matrix) ... Wenn default überschrieben werden soll, muss eine Matrix übergeben werden, aus der hervorgeht, was passieren soll, wenn zu aggregierende Subunits Missings haben
-#     rename (logical)          ... Sollen Units, die nur ein Subunit haben, mit dem Unit- oder dem Subunit-Namen im aggregierten Datensatz stehen (default = FALSE)        
+#     rename (logical)          ... Sollen Units, die nur ein Subunit haben, mit dem Unit- oder dem Subunit-Namen im aggregierten Datensatz stehen (default = FALSE)
 #     recodedData(logical)      ... Soll rekodierter oder Rohdatensatz aggregiert werden (wichtig für Erstellung der Aggregateinfo)
 #
 # Version: 	1.2.0
@@ -26,12 +25,12 @@
 #
 # * 1.2.0 (2011-11-22, NH): bugfix in aggregatemissings
 # * 1.0.0 (2011-11-04, NH): auf ZKD-Inputtabellen angepasst
-#              Scoring komplett rausgenommen  
-#              Option rename eingefügt    
+#              Scoring komplett rausgenommen
+#              Option rename eingefügt
 # * 0.1.1 (2011-10-08, NH): modularisiert, aggregieren und scoren sind getrennte Funktionen
 # * 0.1.0: kein Abbruch bei fehlerhafter Bewertungsvorschrift
 # * 0.0.2: Pattern Matching vektorwertig, kosmetische Änderungen (NH)
-# * 0.0.4: Aggregierung von Missings variabel nach Definition im Deamon 
+# * 0.0.4: Aggregierung von Missings variabel nach Definition im Deamon
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ### TO DO
@@ -39,29 +38,29 @@
 # - wenn rename = TRUE: auch nach unrekodiertem Subitemnamen suchen
 
 aggregateData <- function (dat, subunits, units, aggregatemissings = NULL, rename = FALSE, recodedData = TRUE, verbose = FALSE) {
-  
+
   funVersion <- "aggregateData: "
 
   if (class(dat) != "data.frame") {
   stop(paste(funVersion, "'dat' is not a data.frame.\n", sep = ""))
-  }  
+  }
 
   # make aggregateinfo
   aggregateinfo <- makeInputAggregateData(subunits, units, recodedData = recodedData)
-  
+
   # reduce aggregateinfo - keep only those units for which at least one subunit is found in dat
   nSubunitsInDat <- lapply(lapply(aggregateinfo, "[[", "subunits"), function(ll) { sum( ll %in% colnames(dat)) })
-  aggregateinfo <- aggregateinfo[ which(nSubunitsInDat > 0) ] 
-  
+  aggregateinfo <- aggregateinfo[ which(nSubunitsInDat > 0) ]
+
   if (length(aggregateinfo) == 0){
 	stop("Found none of the specified subitems to aggregate in dataset.\n")
   }
-  
+
   # define missing aggregation
   if ((is.null(aggregatemissings)|is.data.frame(aggregatemissings)|is.matrix(aggregatemissings)) == FALSE){
 	stop("aggregatemissings is neither NULL nor a matrix nor a data.frame.")
-  }	
-  
+  }
+
   if (is.matrix(aggregatemissings)){
 	am <- aggregatemissings
   } else {
@@ -74,54 +73,54 @@ aggregateData <- function (dat, subunits, units, aggregatemissings = NULL, renam
 		am <- matrix(c(
 								"vc" , "mvi", "vc" , "mci", "err", "vc" , "vc" , "err",
 								"mvi", "mvi", "err", "mci", "err", "err", "err", "err",
-								"vc" , "err", "mnr", "mci", "err", "mir", "mnr", "err",           
+								"vc" , "err", "mnr", "mci", "err", "mir", "mnr", "err",
 								"mci", "mci", "mci", "mci", "err", "mci", "mci", "err",
-								"err", "err", "err", "err", "mbd", "err", "err", "err", 
+								"err", "err", "err", "err", "mbd", "err", "err", "err",
 								"vc" , "err", "mir", "mci", "err", "mir", "mir", "err",
-								"vc" , "err", "mnr", "mci", "err", "mir", "mbi", "err", 
+								"vc" , "err", "mnr", "mci", "err", "mir", "mbi", "err",
 								"err", "err", "err", "err", "err", "err", "err", "err" ),
-								nrow = 8, ncol = 8, byrow = TRUE) 
+								nrow = 8, ncol = 8, byrow = TRUE)
 
-		dimnames(am) <- 
-                list(c("vc" ,"mvi", "mnr", "mci",  "mbd", "mir", "mbi", "err"), 
-										c("vc" ,"mvi", "mnr", "mci",  "mbd", "mir", "mbi", "err"))      
+		dimnames(am) <-
+                list(c("vc" ,"mvi", "mnr", "mci",  "mbd", "mir", "mbi", "err"),
+										c("vc" ,"mvi", "mnr", "mci",  "mbd", "mir", "mbi", "err"))
   }
-  
+
   if(!isSymmetric(am)){
 	warning("Matrix used for missing aggregation is not symmetrical. This may lead to unexpected results.")
   }
-	
+
   # which subunits should be aggregated?
   unitsToAggregate <- names(aggregateinfo)
   subunitsToAggregate <- unname(unlist(lapply(aggregateinfo, "[[", "subunits")))
   subunitsToKeep      <- setdiff(colnames(dat), subunitsToAggregate)
-  
+
   # initialisiere aggregierten Datensatz
   datAggregated <- dat[ , subunitsToKeep ]
-  
+
   ### ACHTUNG: IST EHER EXPERIMENTELL!
-  if (rename == TRUE) {    
+  if (rename == TRUE) {
   # prüfen, welche Units nur ein Subunit haben
 	if (recodedData == TRUE) {
 		oneSubunitUnits <- subunits[subunits$subunitRecoded %in% subunitsToKeep, c("unit", "subunitRecoded")]
 		oneSubunitUnits <- oneSubunitUnits [na.omit(match(colnames(dat), oneSubunitUnits$subunitRecoded)), ]
-		colnames(datAggregated)[ match(oneSubunitUnits$subunitRecoded, colnames(datAggregated) )] <- oneSubunitUnits$unit         
+		colnames(datAggregated)[ match(oneSubunitUnits$subunitRecoded, colnames(datAggregated) )] <- oneSubunitUnits$unit
 	} else {
 		oneSubunitUnits <- subunits[subunits$subunit %in% subunitsToKeep, c("unit", "subunit")]
 		oneSubunitUnits <- oneSubunitUnits [na.omit(match(colnames(dat), oneSubunitUnits$subunit)), ]
-		colnames(datAggregated)[ match(oneSubunitUnits$subunit, colnames(datAggregated) )] <- oneSubunitUnits$unit         	
+		colnames(datAggregated)[ match(oneSubunitUnits$subunit, colnames(datAggregated) )] <- oneSubunitUnits$unit
 	}
 	if(verbose) {cat(paste(funVersion, "Found ", nrow(oneSubunitUnits), " unit(s) with only one subunit in 'dat'. This/these subunit(s) will be renamed to their respective unit name(s).\nUnits ",
-          paste(oneSubunitUnits$unit, collapse = ", "), "\n", sep = ""))  }       
-  }  
+          paste(oneSubunitUnits$unit, collapse = ", "), "\n", sep = ""))  }
+  }
 
   # erstelle aggregierten Datensatz der Units, die aggregiert werden
   unitsAggregated <- mapply(aggregateData.aggregate, unitsToAggregate, aggregateinfo, MoreArgs = list(am, dat, verbose = verbose))
-  
+
  if(!missing(unitsAggregated)){
-	datAggregated <- cbind(datAggregated, unitsAggregated, stringsAsFactors = FALSE)	
+	datAggregated <- cbind(datAggregated, unitsAggregated, stringsAsFactors = FALSE)
   }
-  
+
   return(datAggregated)
 }
 
@@ -131,8 +130,8 @@ aggregateData <- function (dat, subunits, units, aggregatemissings = NULL, renam
 .aggmiss <- function ( variable, aggregatedVariable, aggregatemissings) {
 	aggregatedVariable <- mapply( function (variable, aggregatedVariable){
 			x <- aggregatemissings[ match(aggregatedVariable, rownames(aggregatemissings)) , match(variable, colnames(aggregatemissings))]
-		return(x)}, variable, aggregatedVariable)				
-	return (aggregatedVariable) 
+		return(x)}, variable, aggregatedVariable)
+	return (aggregatedVariable)
 }
 
 #-----------------------------------------------------------------------------------------------------------
@@ -141,56 +140,56 @@ aggregateData <- function (dat, subunits, units, aggregatemissings = NULL, renam
 .makeMissingind <- function ( dat, aggregatemissings ) {
 	### wenn gültige Werte, setze sie auf "vc"!
 	dat <- data.frame(apply(dat, 2, function (ll) {gsub("[[:digit:]]", "vc", ll)}), stringsAsFactors = FALSE)
-	
+
 	# initialisiere missing-Rückgabe
 	agg <- dat [ , 1]
-	
+
 	# Missingaggregierung für alle Spalten vornehmen
 	for (i in seq(along = dat)) {
 		agg <- .aggmiss(dat [ , i] , agg, aggregatemissings)
 	}
 	return(agg)
-}	
-	
+}
+
 #-----------------------------------------------------------------------------------------------------------
 # findet zu aggregierende Spalten eines Datensatzes und aggregiert sie nach einer vorgegebenen Aggregierungsregel
 
 aggregateData.aggregate <- function(unitName, aggregateinfo, aggregatemissings, dat, verbose = FALSE){
 
   funVersion <- "aggregateData: "
-  
+
   unitVars <- aggregateinfo$subunits
   aggRule <- toupper(aggregateinfo$arule)
-  
+
   if(verbose) cat(paste (funVersion, "Aggregate unit ", unitName, "", sep = ""))
-  
-  # check: sind alle Subunits vorhanden?	
+
+  # check: sind alle Subunits vorhanden?
   if (any((unitVars %in% colnames(dat)) == FALSE)) {
   	stop(paste(funVersion, "Subunits", paste(setdiff(unitVars, colnames(dat)), collapse = ", "), "not in 'dat'.\n"))
-  } 
-  
+  }
+
   # gebe Teildatensatz für i-tes unit
   unitDat        <- dat[ , unitVars]
-  
+
   # check: Datensatz darf keine NAs enthalten. -> werden in mbd umgewandelt
   na <- which(is.na(unitDat))
     if (length(na) > 0) {
         if(verbose)  cat(paste(funVersion, "Data contains NA values. These values will be converted to 'mbd'.\n", sep = ""))
 		unitDat[ is.na(unitDat) ] <- "mbd"
-  } 
-  
+  }
+
   agg <- .makeMissingind(unitDat, aggregatemissings)
-  
+
   ### wenn agg an irgendeiner Stelle auf "err" gesetzt wird, muß die Funktion mit einer Fehlermeldung abbrechen!
   if(any(agg == "err"))  {
   	stop(paste(funVersion, "Aggregation of missing values for unit ", unitName, " produced 'err'.\n",sep=""))
-  }	
-  	
+  }
+
   # initialisiere Rückgabe des aggregierten units
   unitAggregated <- unname(agg)
-  
-  options(warn = -1)	
-  
+
+  options(warn = -1)
+
   ### MH 19.03.2015
   # Behandlung der Aggregation Rule
   if ( !exists ( "aggRule" ) ) {
@@ -199,14 +198,17 @@ aggregateData.aggregate <- function(unitName, aggregateinfo, aggregatemissings, 
   if ( is.na ( aggRule ) ) {
 		aggRule <- "SUM"
   }
+  if (nchar(aggRule) == 0) {
+    aggRule <- "SUM"
+  }
   if ( !is.character ( aggRule ) ) {
 		aggRule <- "SUM"
   }
   # Warnung wenn nicht eine standardmäßige aggRule
   if ( !aggRule %in% c("SUM","MEAN","") ) {
 		warning ( paste ( "Unit " , unitName , " has potentially problematic aggregation rule (\"" , aggRule , "\"). Please check.\n" , sep = "" ) )
-  }  
-  
+  }
+
   # MH 10.01.2013
   # Hotfix:
   # wenn die aggRule eine aggRule ist,
@@ -216,7 +218,7 @@ aggregateData.aggregate <- function(unitName, aggregateinfo, aggregatemissings, 
   # alternativ könnte man das auch gleich im ZKDaemon ändern,
   # also dass wenn die aggRule nach unitRecoding umgesetzt wurde,
   # diese in units$unitAggregateRule gelöscht oder auf SUM gesetzt wird
-  # die Erkennung der "bekannten" Regel ist hier sehr primitiv und 
+  # die Erkennung der "bekannten" Regel ist hier sehr primitiv und
   # nicht sonderlich erschöpfend/fehlersicher, müsste optimiert werden
   # if ( nchar (aggRule) > 0 ) {
 		# if ( grepl ( ":" , aggRule , fixed = TRUE ) ) aggRule <- "SUM"
@@ -225,7 +227,7 @@ aggregateData.aggregate <- function(unitName, aggregateinfo, aggregatemissings, 
   # if ( !aggRule %in% c("SUM","MEAN","") ) {
 		# warning ( paste ( "Unit " , unitName , " has potentially problematic aggregation rule (\"" , aggRule , "\"). Please check.\n" , sep = "" ) )
   # }
-  
+
   # Aggregierung des units je nach Regel
 
   # MH 11.01.2013
@@ -236,11 +238,11 @@ aggregateData.aggregate <- function(unitName, aggregateinfo, aggregatemissings, 
 		  if( aggRule == "SUM" ) {
 			unitAggregated[unitAggregated == "vc"] <- as.character(rowSums(apply(unitDat.vc, 2, as.numeric), na.rm = TRUE))
 		  }
-		  
+
 		  if( aggRule == "MEAN" ) {
-			unitAggregated[unitAggregated == "vc"] <- as.character(rowMeans(apply(unitDat.vc, 2, as.numeric), na.rm = TRUE)) 
+			unitAggregated[unitAggregated == "vc"] <- as.character(rowMeans(apply(unitDat.vc, 2, as.numeric), na.rm = TRUE))
 		  }
-  } 
+  }
   #else {
   #		unitAggregated <- unitDat
   #		cat ( paste ( "No valid cells (only missings) for " , unitName , "\n" , sep = "" ) )
@@ -248,18 +250,18 @@ aggregateData.aggregate <- function(unitName, aggregateinfo, aggregatemissings, 
   # MH 14.01.2013
   # der Teil war etwas suboptimal
   # da die vorige missing aggregation wieder überschrieben wurd :-)
-  
+
   # MH 15.03.2015
   # so viele Punkte printen wie Anzahl an Subitems
   if ( verbose ) cat ( paste0 ( paste ( rep ( "." , ncol ( unitDat.vc ) ) , collapse = "" ) , "\n" ) )
-  
+
   options(warn = 0)
-  
+
   # pattern aggregation (noch nicht getestet)
   #	if( !aggRule %in% c("SUM","MEAN") )  {
   #	unit.pattern <- apply( unitDat[ unit == "vc" ], 1, paste, collapse = "")
   #	unit [ unit == "vc" ] <- recode(unit.pattern, aggRule)
   #	}
-  
+
   return(unitAggregated)
 }
--- pkg/eatPrep/R/cat.pbc.R
+++ pkg/eatPrep/R/cat.pbc.R
@@ -1,6 +1,6 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # catPbc
-# Description: berechnet Kategorientrennschärfen 
+# Description: berechnet Kategorientrennschärfen
 # Version: 	0.1.0
 # Status: alpha
 # Release Date: 2012-06-11, 2012-07-27 implemented in eat
@@ -8,13 +8,13 @@
 #
 # Change Log:
 # 2012-08-28 NH
-# ADDED: catPbc provides information about all valid codes 
+# ADDED: catPbc provides information about all valid codes
 # 2012-08-23 NH
 # ADDED: Argument 'context.vars' in catPbc
 # 0000-00-00 AA
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-## TO DO: 
+## TO DO:
 ## auch mit aggregiertem und bewertetem Datensatz nutzbar machen (Wunsch Alex Roppelt)
 ## vgl. recodeData(dat=dat, values=inputList$unitRecodings, subunits=inputList$units)
 
@@ -36,36 +36,42 @@ catPbc <- function(datRaw, datRec, idRaw, idRec, context.vars, values, subunits,
 	idrec <- datRec [ , idRec ]
 	idraw <- datRaw  [ , idRaw ]
 	if ( ! setequal(idrec, idraw) ) {
-		stop ( "catPbc: IDs in datasets are not all identical." ) 
+		stop ( "catPbc: IDs in datasets are not all identical." )
 	} else {
 
-		# sort IDs 
+		# sort IDs
 		datRec  <- datRec [ order(idrec) , ]
-		datRaw   <- datRaw  [ order(idraw) , ]   
+		datRaw   <- datRaw  [ order(idraw) , ]
+		
+		# exclude context vars from subunits and values
+  if ( length(context.vars != 0) ) {
+    subunits <- subunits[-which(subunits$subunit %in% context.vars), ]
+    values   <- values[-which(values$subunit %in% context.vars), ]
+	}
 
     # make inputs
-    recodeinfo <- makeInputRecodeData (values = values, subunits = subunits)
-    varinfo    <- .makeVarinfoRaw (values, subunits)
-    
+    recodeinfo <- eatPrep:::makeInputRecodeData (values = values, subunits = subunits)
+    varinfo    <- eatPrep:::.makeVarinfoRaw (values, subunits)
+
 #		Kontextvariablen ausschließen
     if(is.numeric(context.vars)) {
       context.vars <- colnames(datRaw)[context.vars]
     }
     keep.varsRaw <- setdiff(colnames(datRaw), context.vars)
     keep.varsRec <- setdiff(colnames(datRec), c(context.vars, paste(context.vars, "R", sep = "")))
-    
+
     datRaw <- datRaw[ , match(keep.varsRaw, colnames(datRaw))]
     datRec <- datRec[ , match(keep.varsRec, colnames(datRec))]
-    
+
     if(is.numeric(idRaw)) {
       idRaw <- colnames(datRaw)[idRaw]
     }
     vars <- setdiff(colnames(datRaw), idRec)
-		
+
     # relative Punktzahl
 		options(warn=-1)
 		rel.score1 <- datRec[ , which( colnames(datRec) %in% subunits$subunitRecoded ) ]
-		rel.score1 <- apply(rel.score1, 2, as.numeric ) 
+		rel.score1 <- apply(rel.score1, 2, as.numeric )
 		rel.score <- rowMeans(  rel.score1, na.rm=T )
 
 		# Kategorientrennschärfen berechnen
@@ -79,7 +85,7 @@ catPbc <- function(datRaw, datRec, idRaw, idRec, context.vars, values, subunits,
 			validCodes <- names(valueTypes)  [ valueTypes == "vc" ]
       valuesToNA <- c( "mbd", "mci", names(valueTypes) [ valueTypes %in% c("mbd", "mci") ] )
 			dat.vv [ dat.vv %in% valuesToNA] <- NA
-			
+
 			# Häufigkeitsverteilung der Codes, ohne mbd & mci
 			tvv <- table( dat.vv )
 			kat.vv <- names(tvv)
@@ -90,13 +96,13 @@ catPbc <- function(datRaw, datRec, idRaw, idRec, context.vars, values, subunits,
         tvv <- c(tvv, addCodes)
         tvv <- tvv[order(names(tvv))]
         kat.vv <- sort(names(tvv))
-      } 
+      }
 			tvv1 <- tvv / sum(tvv)
 
 			for (bb in seq ( along = kat.vv) ){
 				#                bb <- 3
-				l.bb <- c( var.vv  , kat.vv[bb] , sum(tvv) , tvv[bb] , tvv1[bb] , 
-				  cor( 1 * ( dat.vv == kat.vv[bb] ) , rel.score , use = "complete.obs" ) , 
+				l.bb <- c( var.vv  , kat.vv[bb] , sum(tvv) , tvv[bb] , tvv1[bb] ,
+				  cor( 1 * ( dat.vv == kat.vv[bb] ) , rel.score , use = "complete.obs" ) ,
 				  recodeinfo[[ var.vv ]]$values [[kat.vv[bb]]]
 				  )
 				dfr1 <- rbind( dfr1 , l.bb )
@@ -104,14 +110,14 @@ catPbc <- function(datRaw, datRec, idRaw, idRec, context.vars, values, subunits,
 		}
 		dfr1 <- data.frame(dfr1, stringsAsFactors = FALSE)
 		colnames(dfr1) <- c( "item" , "cat" , "n" , "freq" , "freq.rel" , "catPbc" , "recodevalue" )
-		dfr1 [ , 3:6 ] <- apply(dfr1 [ , 3:6 ], 2, as.numeric ) 
+		dfr1 [ , 3:6 ] <- apply(dfr1 [ , 3:6 ], 2, as.numeric )
 		dfr2 <- merge(dfr1, subunits[ , c("subunit", "subunitType")], by.x = "item", by.y = "subunit",  all.x = T)
-		
+
 	}
-	
+
 	if ( !is.null(xlsx)) {
 			try(write.xlsx(dfr2, file = xlsx, sheetName="catPbc", col.names=TRUE, row.names=TRUE, append=FALSE))
-	}	
-	
-	return(dfr2) 
+	}
+
+	return(dfr2)
 }
