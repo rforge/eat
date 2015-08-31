59ee7325e9fdfdfba70c1e7521ae901781ef654e
atout <atout@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-31 11:56:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/eat@486 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/eatPrep/R/mergeData.R
+++ pkg/eatPrep/R/mergeData.R
@@ -1,71 +1,113 @@
 mergeData <- function ( newID="ID", datList, oldIDs=NULL, addMbd = FALSE, verbose=FALSE) {
-  versNr <- "0.8.0"
+  versNr <- "0.6.0"
   mReturn <- NULL
   stopifnot (is.list (datList))
   if(is.null(oldIDs)) {oldIDs <- rep(newID, length(datList))}
   for ( i in seq(along = datList)) {stopifnot(is.data.frame(datList[[i]]))} 
   for ( i in seq(along = datList)) {stopifnot( oldIDs[i] %in% colnames(datList[[i]]) )} 
   if ( length(datList) > 0 ) {
-	
+	fkNam <- list()
+	for ( i in seq(along = datList)) {
+		if(any(unlist(lapply(datList[[i]], class)) == "factor")) {
+			fkNam[[i]] <- names(which(unlist(lapply(datList[[i]], class)) == "factor"))
+			datList[[i]] <- set.col.type(datList[[i]], col.type = list ( "character" = fkNam[[i]] ))
+		}
+	}
     stopifnot(length(datList) == length(oldIDs))
     stopifnot(is.numeric(oldIDs) | is.character(oldIDs))
-	
-	datList <- mapply(function(dat, ids, i) {
-		if (!(ids %in% colnames(dat))) {
-			stop(paste ( "mergeData_", versNr, ": Didn't find ID variable '", ids, "' in dataset ", i, "\n", sep = ""))
-		}
-		if ( is.numeric( ids )) {
-			colnames(dat)[ids] <- newID
-			}		
-		if ( is.character( ids )) {
-			colnames(dat)[match(ids,colnames(dat))] <- newID
-			}
-		if (length(na.omit(dat[, newID])) != length(na.omit (unique(dat[, newID])))) { 
-			doppelt <- na.omit(unique(dat[,newID][ duplicated(dat[,newID])]))
-			cat(paste("mergeData_", versNr, ": Multiple ID(s) in dataset ", i, " in " ,length(doppelt), " case(s). \n",sep=""))
-			warning(cat(paste("Multiple ID(s): ",paste(doppelt, collapse = ", "), "\n" )))}
-		return(dat)
-	}, datList, oldIDs, seq(along=datList))
-	
-### SW, 1. Oktober 2014:
-### datList <- lapply(datList, reshape2::melt, id=newID)
-	datList <- lapply(datList, FUN = function ( l ) { reshape2::melt( data = l, id.vars = newID) })
-	
-	for(i in seq(along=datList)) {
-		if(i==1) {datLong <- datList[[i]]} else {
-			datLong <- rbind(datLong, datList[[i]])
-		}
-	}
-	datLong <- set.col.type(datLong, col.type=list("character" = names(datLong)))
-	
-	datLong <- datLong[-which(is.na(datLong$value)),]
-
-	if(any(duplicated(paste(datLong[,newID], datLong$variable)))) {
-		if(any(duplicated(paste(datLong[,newID], datLong$variable, datLong$value)))) {
-			datLong <- datLong[-which(duplicated(paste(datLong[,newID], datLong$variable, datLong$value))),] 
-		} 
-		if(any(duplicated(paste(datLong[,newID], datLong$variable)))) {
-			doppi <- unique(c(which(duplicated(paste(datLong[,newID], datLong$variable))), which(duplicated(paste(datLong[,newID], datLong$variable), fromLast=TRUE))))
-			doppi <- doppi[order(doppi)]
-			tt <- NULL
-			for(ll in unique(datLong$variable[doppi])) {
-				warning(paste("mergeData_", versNr, ": Multiple different valid codes in variable: ", ll, ", ID: ", unique(datLong[doppi,][which(datLong$variable[doppi] %in% ll),1]), ", Codes: ", paste(unique(datLong[doppi,][which(datLong$variable[doppi] %in% ll),3]), collapse = ", "), " \n The first value will be kept.\n", sep = ""))
-				tt <- c(tt, which(paste(datLong[,1], datLong[,2], datLong[,3]) %in% paste(datLong[doppi,][which(datLong$variable[doppi] %in% ll),][-1,], collapse=" ")))
-			}	
-			datLong <- datLong[-tt,]
-		}
-	}
-### SW, 1. Oktober 2014:
-### mReturn <- reshape2::dcast(datLong, add.missing =TRUE)
-	mReturn <- dcast(datLong, as.formula(paste( newID,"~variable",sep="")), value.var = "value")
-	mReturn <- set.col.type(mReturn, col.type=list("character" = names(mReturn)))
-	mReturn <- data.frame(mReturn)
-	if(addMbd) {mReturn[is.na(mReturn)] <- "mbd"}
+		for ( i in seq(along = datList)) {
+		  if(verbose) {cat(paste ( "mergeData_", versNr, ": Start merging of dataset ", i, ".\n", sep=""))}
+			if ( i == 1 ) {
+				if ( is.character( oldIDs )) {	IDname1 <- oldIDs[i] }
+				if ( is.numeric( oldIDs )) { IDname1 <- colnames(datList[[i]])[oldIDs[i]]   }
 		
+				if ( length( na.omit( datList[[i]][ , IDname1])) != length( na.omit ( unique( datList[[i]][ , IDname1] ) ) ) ) {
+							doppelt <- na.omit( unique( datList[[i]][ , IDname1][ duplicated(datList[[i]][ , IDname1] ) ] ) )
+							cat(paste ( "mergeData_", versNr, ": Multiple IDs in dataset ", i, " in " , length(doppelt)," cases. \n",sep=""))
+							stop( cat(paste ( "Multiple IDs: ", paste( doppelt, collapse = ", "), "\n" )))}
+				if ( is.character(IDname1)) {
+					names(datList[[i]])[names(datList[[i]]) == IDname1] <- newID  
+					mergedData <- datList[[i]]
+				} else {
+					warning(paste ( "mergeData_", versNr, ": Found no ID variable in dataset", i, "\n"))
+					mReturn <- FALSE
+				}
+			} else {
+
+        if ( is.character( oldIDs )) {	IDname2 <- oldIDs[i] }
+        if ( is.numeric( oldIDs )) { IDname2 <- colnames(datList[[i]])[oldIDs[i]]   }
+				if ( is.character(IDname2)) {
+					names(datList[[i]])[names(datList[[i]]) == IDname2] <- newID 
+					partialData <- merge( mergedData, datList[[i]], all = TRUE )
+					colID <- match(newID, colnames(partialData))
+#					partialData <- partialData[order(partialData[colID]),]
+
+					# wenn mehrere Einträge mit gleicher ID gefunden wurden.... 
+					if( length(na.omit ( partialData[[colID]]) ) != length(na.omit ( unique(partialData[[colID]])) ) ) {
+						
+						# total columns and rows for the targetData dataframe
+						total.row <- length(na.omit ( unique(partialData[[colID]])) )
+						total.col <- ncol(partialData)
+						row.ID <- na.omit ( unique(partialData[[colID]]) )
+						targetData <- matrix(NA,total.row,total.col)
+						targetData <- as.data.frame(targetData)
+						names(targetData) <- names(partialData)
+						
+						# looking for multiple cases
+						multipleCases <- na.omit ( unique ( partialData[[ colID ]] [ duplicated(partialData[[ colID ]]) ] )  )
+						
+						for (k in seq( along = multipleCases ) ){
+							# select all rows with the same ID
+							partialData.part <- partialData[ which ( partialData[colID] == multipleCases[k] ),]             
+							targetData [ k , ] <- partialData.part [ 1 , ]
+							# in welchen Spalten treten (mindestens) zwei unterschiedliche Werte auf, inkl. Missings?
+							differentCol <- which ( sapply ( partialData.part , function (ll) { length (table(ll, useNA = "ifany")) > 1 }) )
+             
+							for ( j in differentCol ){
+								# Wenn es mehr als einen Nicht-NA-Eintrag gibt:
+								if( sum( ! is.na(partialData.part[ ,j]) ) > 1 ) {
+								  # nimm den ersten davon, wenn alle gleich sind
+								  if ( length( table (partialData.part [ , j ])) == 1 ) {
+                    targetData[ k, j] <- partialData.part [ which( !is.na (partialData.part[ , j] ) )[1], j ] 
+                  } else {
+  									# nimm den ersten davon und gib eine Warnung aus, wenn nicht alle gleich sind
+					warning(paste("mergeData_", versNr, ": Multiple different valid codes in variable ", colnames(targetData)[j],
+  										  ", ID ", multipleCases [k], ", Codes: ", paste(na.omit ( partialData.part[ 1,j] ), " & ", na.omit ( partialData.part[ 2,j] ),".", sep=""),
+  										  " \n The first value will be kept.\n", sep = ""))
+  									targetData[ k, j] <- partialData.part [ which( !is.na (partialData.part[ , j] ) )[1], j ]
+                  }    
+                } else {
+  								# wenn es genau einen Nicht-NA-Eintrag gibt, dann nimm den
+  								if (sum( ! is.na(partialData.part[ ,j]) ) == 1 ) {
+  									targetData[ k, j] <- partialData.part [ which( !is.na (partialData.part[ , j] ) ), j ]
+  								} 
+  							}	
+              }
+  				}
+					# Daten für nur einmal vorkommende IDs in targetData schreiben
+					ind <- seq ( length(multipleCases)+1,  nrow(targetData), 1)		
+					targetData[ ind ,  ] <- partialData[ which( ! partialData[[colID]] %in% multipleCases ),  ]	
+					} else {
+						# Wenn keine mehrfachen IDs vorkommen, dann einfach gemergten Datensatz übernehmen
+						targetData <- partialData
+					}
+					mergedData <- targetData
+				} else {
+					warning(paste ( "mergeData_", versNr, ": Found no ID variable in dataset", i, "\n"))
+					mReturn <- FALSE
+				}			
+			}
+		mReturn <- mergedData
+		if(addMbd == TRUE) {mReturn[is.na(mReturn)] <- "mbd"}
+		}
 	} else {    
 		warning(paste("mergeData_", versNr, ": Found no datasets."), "\n")
 		mReturn <- FALSE
 	}
-	return(mReturn)
+	if(is.data.frame(mReturn)) {
+		if(!is.null(unlist(fkNam))) {
+			mReturn <- set.col.type(mReturn, col.type = list ( "factor" = unique(unlist(fkNam))))
+		}
+	}
+	return (mReturn)
 }
-
