0b778d46ab822e4657b17c63b61d2ee8aa579aec
martinhecht <martinhecht@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-12-13 13:36:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/eat@660 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/ctglm/R/merge.results.R
@@ -0,0 +1,73 @@
+
+merge.results <- function( ..., consistent=TRUE, noNAcol=TRUE ) {
+
+		# arguments from ...
+		# if( length( list(...) ) > 0 ) {
+				# eval( parse ( text=paste0( "assign( '",names(list(...)), "' , list(...)$'",names(list(...)),"' , envir=env )" ) ) )
+		# }
+		arg <- list(...)
+# browser()		
+		## identify if
+		# folder
+		fols <- arg[ sapply( arg, dir.exists ) ]
+		# TODO: results
+		# TODO: list of results
+		
+		
+		### folders
+		if ( length( fols ) > 0 ) {
+				
+				fls.l <- sapply( fols, list.files, pattern="\\.[Rdata$|rda$]", full.names=TRUE, simplify=FALSE )
+				fls <- do.call( "rbind", fls.l )
+				# !!!!!!!!temp!!!!!!!!!
+				# fls <- fls[1:10]
+
+				load.fls <- function( fl, verbose=TRUE ) {
+				# browser()
+						# output
+						if( verbose ){
+								cat( paste0( fl, "" ) ); flush.console()
+						}
+						
+						# load file
+						d.nam <- load( fl )
+						# TODO: check all objects in file, not only first
+						d <- get( d.nam[1] )
+						
+						# identify if its results object
+						if( is.data.frame( d ) && all( c( "model.name","engine","name","variable","value" ) %in% colnames( d ) ) ) {
+								ret <- d
+								if( verbose ) cat( " -> added to results\n" )
+						} else {
+								ret <- NULL
+								if( verbose ) cat( " -> NO results\n" )
+						}
+						
+						return( ret )
+				}
+				d.l <- sapply( fls, load.fls, simplify=FALSE )
+				d <- do.call( "rbind", d.l )
+				rownames( d ) <- seq( along=rownames( d ) )
+
+				# keep only parameter that are in all analyses
+				if ( consistent ) {
+						var.nams <- sapply( d.l, function( d ) unique( d$variable ), simplify=FALSE )
+						var.nams <- var.nams[!sapply(var.nams,is.null)]
+						vars <- Reduce(function(x, y) intersect(x, y),var.nams,accumulate=FALSE )
+
+						d <- d[ d$variable %in% vars, ]
+				}
+
+				# delete columns with NA
+				if ( noNAcol ){
+						del <- sapply( d, function( cl ) any( is.na( cl ) ) )
+						d <- d[,!del,drop=FALSE]
+				}
+		} else {
+				d <- NULL
+		} ### end of folders
+		
+		
+		
+		return( d )
+}
