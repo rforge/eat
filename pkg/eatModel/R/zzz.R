dc276bc4fd3bc64462202c75f361d1cb0f057ba1
atout <atout@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-02-21 07:42:01 +0000

    weiter eatModel
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/eat@771 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/eatModel/R/zzz.R
+++
@@ -1,6 +0,0 @@
-
-.onAttach <- function(lib, pkg){
-	welcomeMsg <- "\n==============================================\n    This eatModel version is deprecated.\n   Latest version can be found on GitHub.\n                   Please use\n> devtools::install_github(\"weirichs/eatModel\")\n          to install latest version.\n==============================================\n" 
-	packageStartupMessage ( welcomeMsg )
-	#invisible ( welcomeMsg )
-}
